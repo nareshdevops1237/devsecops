7d5ad48bfb38f968bac11d4a5f7cc57a9b2d88e0
Naresh Chintalapudi <nareshdevops1237@gmail.com>
2026-07-31 21:24:20 +0300

    added .gitignore file

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_network_interface" "app_nic" {
   name                = "app-nic"
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
-  
+
 
   ip_configuration {
     name                          = "app-ip"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region for all resources"
   type        = string
-  default     = "eastus2"
+  default     = "eastus"
 }
 
 
